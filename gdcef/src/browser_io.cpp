3fe49f1392554e8f27838e5ef0483a90a1fe55ab
Quentin Quadrat <lecrapouille@gmail.com>
2022-11-14 22:20:58 +0100

    mouseWheel scrolled vertically and horizontally. Separate !

--- gdcef/src/browser_io.cpp
+++ gdcef/src/browser_io.cpp
@@ -148,7 +148,7 @@ void GDBrowserView::mouseMove(int x, int y)
 }
 
 //------------------------------------------------------------------------------
-void GDBrowserView::mouseWheel(const int wDelta)
+void GDBrowserView::mouseWheelVertical(const int wDelta)
 {
     if (m_browser == nullptr)
         return ;
@@ -157,7 +157,20 @@ void GDBrowserView::mouseWheel(const int wDelta)
     evt.x = m_mouse_x;
     evt.y = m_mouse_y;
 
-    m_browser->GetHost()->SendMouseWheelEvent(evt, wDelta * 10, wDelta * 10);
+    m_browser->GetHost()->SendMouseWheelEvent(evt, 0, wDelta * 10);
+}
+
+//------------------------------------------------------------------------------
+void GDBrowserView::mouseWheelHorizontal(const int wDelta)
+{
+    if (m_browser == nullptr)
+        return ;
+
+    CefMouseEvent evt;
+    evt.x = m_mouse_x;
+    evt.y = m_mouse_y;
+
+    m_browser->GetHost()->SendMouseWheelEvent(evt, wDelta * 10, 0);
 }
 
 //------------------------------------------------------------------------------
